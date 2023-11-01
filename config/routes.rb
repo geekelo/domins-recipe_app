e97836f1b562b549d05485277e5e880cc26c2734
geekelo <efueliht12@gmail.com>
2023-11-01 12:33:06 +0100

    Add sign out feature

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "public_recipes#index"
 end
