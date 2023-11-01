afa9b3a876316ab8a506c82add7210fbba3ea4a4
geekelo <efueliht12@gmail.com>
2023-11-01 14:22:08 +0100

    Implement Add, list and delete food items by logged in users

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,32 @@
+class FoodsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @foods = current_user.foods
+  end
+
+  def new
+    @food = current_user.foods.build
+  end
+
+  def create
+    @food = current_user.foods.build(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food item successfully added'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @food = current_user.foods.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'Food item deleted successfully'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :quantity, :price)
+  end
+end
