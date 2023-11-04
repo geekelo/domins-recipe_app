5d86f56bc84c4b581f63d6aa43187bade9ae5c48
geekelo <efueliht12@gmail.com>
2023-11-04 22:37:23 +0100

    Add style rules for recipe show page

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,8 +1,13 @@
-def index
-  @recipe_foods = RecipeFood.joins(:food)
-    .select('foods.name,
-                                     SUM(recipe_foods.quantity) as total_quantity,
-                                     SUM(recipe_foods.value) as total_value')
-    .group('foods.name')
-    .includes(:food)
+class GeneralShoppingListsController < ApplicationController
+  def index
+    @recipe_foods = Food.joins(
+      "INNER JOIN (SELECT food_id, 
+                        SUM(quantity) as total_quantity, 
+                        SUM(value) as total_value 
+                 FROM recipe_foods GROUP BY food_id) as subquery 
+       ON foods.id = subquery.food_id")
+      .select('foods.name, 
+               subquery.total_quantity, 
+               subquery.total_value')
+  end  
 end
