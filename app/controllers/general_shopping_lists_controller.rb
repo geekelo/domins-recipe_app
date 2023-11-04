392758ed87d0e911a818d65dd5e8c92d5c936aa4
geekelo <efueliht12@gmail.com>
2023-11-04 22:40:49 +0100

    Fix linter errors

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,13 +1,14 @@
 class GeneralShoppingListsController < ApplicationController
   def index
     @recipe_foods = Food.joins(
-      "INNER JOIN (SELECT food_id, 
-                        SUM(quantity) as total_quantity, 
-                        SUM(value) as total_value 
-                 FROM recipe_foods GROUP BY food_id) as subquery 
-       ON foods.id = subquery.food_id")
-      .select('foods.name, 
-               subquery.total_quantity, 
+      "INNER JOIN (SELECT food_id,
+                        SUM(quantity) as total_quantity,
+                        SUM(value) as total_value
+                 FROM recipe_foods GROUP BY food_id) as subquery
+       ON foods.id = subquery.food_id"
+    )
+      .select('foods.name,
+               subquery.total_quantity,
                subquery.total_value')
-  end  
+  end
 end
