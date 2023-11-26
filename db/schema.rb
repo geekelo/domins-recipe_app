8c1194e7adcc7622bbbc349a14e0392390a14dab
geekelo <efueliht12@gmail.com>
2023-11-26 23:39:24 +0100

    Deploy ap on render

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_03_160321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
@@ -33,26 +23,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_03_160321) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.integer "comment_counter", default: 0
-    t.integer "like_counter", default: 0
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
@@ -95,9 +65,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_03_160321) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
   add_foreign_key "foods", "users"
-  add_foreign_key "likes", "posts"
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipe_foods", "recipes", on_delete: :cascade
